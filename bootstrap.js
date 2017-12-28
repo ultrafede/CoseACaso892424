abb75a850aba5fe093346f4e35192338e6737c1d
drfede <34274861+drfede@users.noreply.github.com>
2017-12-28 12:11:52 +0100

    455

--- bootstrap.js
+++ bootstrap.js
@@ -353,106 +353,106 @@ if (typeof jQuery === 'undefined') {
 
 +function ($) {
   Carousel.prototype.keydown = function (e) {
-    if (/input|textarea/i.test(e.target.tagName)) return
+    if (/input|textarea/i.test(e.target.tagName)) return;
     switch (e.which) {
-      case 37: this.prev(); break
-      case 39: this.next(); break
-      default: return
+      case 37: this.prev(); break;
+      case 39: this.next(); break;
+      default: return;
     }
 
-    e.preventDefault()
+    e.preventDefault();
   }
 }
 
 +function ($) {
   Carousel.prototype.cycle = function (e) {
-    e || (this.paused = false)
+    e || (this.paused = false);
 
-    this.interval && clearInterval(this.interval)
+    this.interval && clearInterval(this.interval);
 
     this.options.interval
       && !this.paused
-      && (this.interval = setInterval($.proxy(this.next, this), this.options.interval))
+      && (this.interval = setInterval($.proxy(this.next, this), this.options.interval));
 
-    return this
+    return this;
   }
 }
 
 +function ($) {
   Carousel.prototype.getItemIndex = function (item) {
-    this.$items = item.parent().children('.item')
-    return this.$items.index(item || this.$active)
+    this.$items = item.parent().children('.item');
+    return this.$items.index(item || this.$active);
   }
 }
 
 +function ($) {
   Carousel.prototype.getItemForDirection = function (direction, active) {
-    var activeIndex = this.getItemIndex(active)
+    var activeIndex = this.getItemIndex(active);
     var willWrap = (direction == 'prev' && activeIndex === 0)
-                || (direction == 'next' && activeIndex == (this.$items.length - 1))
-    if (willWrap && !this.options.wrap) return active
-    var delta = direction == 'prev' ? -1 : 1
-    var itemIndex = (activeIndex + delta) % this.$items.length
-    return this.$items.eq(itemIndex)
+                || (direction == 'next' && activeIndex == (this.$items.length - 1));
+    if (willWrap && !this.options.wrap) return active;
+    var delta = direction == 'prev' ? -1 : 1;
+    var itemIndex = (activeIndex + delta) % this.$items.length;
+    return this.$items.eq(itemIndex);
   }
 }
 
 +function ($) {
   Carousel.prototype.to = function (pos) {
-    var that        = this
-    var activeIndex = this.getItemIndex(this.$active = this.$element.find('.item.active'))
+    var that        = this;
+    var activeIndex = this.getItemIndex(this.$active = this.$element.find('.item.active'));
 
-    if (pos > (this.$items.length - 1) || pos < 0) return
+    if (pos > (this.$items.length - 1) || pos < 0) return;
 
-    if (this.sliding)       return this.$element.one('slid.bs.carousel', function () { that.to(pos) }) // yes, "slid"
-    if (activeIndex == pos) return this.pause().cycle()
+    if (this.sliding)       return this.$element.one('slid.bs.carousel', function () { that.to(pos) }); // yes, "slid"
+    if (activeIndex == pos) return this.pause().cycle();
 
-    return this.slide(pos > activeIndex ? 'next' : 'prev', this.$items.eq(pos))
+    return this.slide(pos > activeIndex ? 'next' : 'prev', this.$items.eq(pos));
   }
 }
 
 +function ($) {
   Carousel.prototype.pause = function (e) {
-    e || (this.paused = true)
+    e || (this.paused = true);
 
     if (this.$element.find('.next, .prev').length && $.support.transition) {
-      this.$element.trigger($.support.transition.end)
-      this.cycle(true)
+      this.$element.trigger($.support.transition.end);
+      this.cycle(true);
     }
 
-    this.interval = clearInterval(this.interval)
+    this.interval = clearInterval(this.interval);
 
-    return this
+    return this;
   }
 }
 
 +function ($) {
   Carousel.prototype.next = function () {
-    if (this.sliding) return
-    return this.slide('next')
+    if (this.sliding) return;
+    return this.slide('next');
   }
 
   Carousel.prototype.prev = function () {
-    if (this.sliding) return
-    return this.slide('prev')
+    if (this.sliding) return;
+    return this.slide('prev');
   }
 }
 
 +function ($) {
   Carousel.prototype.slide = function (type, next) {
-    var $active   = this.$element.find('.item.active')
-    var $next     = next || this.getItemForDirection(type, $active)
-    var isCycling = this.interval
-    var direction = type == 'next' ? 'left' : 'right'
-    var that      = this
+    var $active   = this.$element.find('.item.active');
+    var $next     = next || this.getItemForDirection(type, $active);
+    var isCycling = this.interval;
+    var direction = type == 'next' ? 'left' : 'right';
+    var that      = this;
 
-    if ($next.hasClass('active')) return (this.sliding = false)
+    if ($next.hasClass('active')) return (this.sliding = false);
 
-    var relatedTarget = $next[0]
+    var relatedTarget = $next[0];
     var slideEvent = $.Event('slide.bs.carousel', {
       relatedTarget: relatedTarget,
       direction: direction
-    })
+    });
     this.$element.trigger(slideEvent)
     if (slideEvent.isDefaultPrevented()) return
 
