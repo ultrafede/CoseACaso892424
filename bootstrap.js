9575143e0ca464d1573f92db292c1cb4e4f73ef0
drfede <34274861+drfede@users.noreply.github.com>
2017-12-28 11:18:03 +0100

    250

--- bootstrap.js
+++ bootstrap.js
@@ -167,7 +167,7 @@ if (typeof jQuery === 'undefined') {
   // ALERT DATA-API
   // ==============
 
-  $(document).on('click.bs.alert.data-api', dismiss, Alert.prototype.close)
+  $(document).on('click.bs.alert.data-api', dismiss, Alert.prototype.close);
 
 }(jQuery);
 
@@ -187,67 +187,67 @@ if (typeof jQuery === 'undefined') {
   // ==============================
 
   var Button = function (element, options) {
-    this.$element  = $(element)
-    this.options   = $.extend({}, Button.DEFAULTS, options)
-    this.isLoading = false
+    this.$element  = $(element);
+    this.options   = $.extend({}, Button.DEFAULTS, options);
+    this.isLoading = false;
   }
 }
 
 +function ($) {
-  Button.VERSION  = '3.3.7'
+  Button.VERSION  = '3.3.7';
 
   Button.DEFAULTS = {
     loadingText: 'loading...'
-  }
+  };
 }
 
 +function ($) {
   Button.prototype.setState = function (state) {
-    var d    = 'disabled'
-    var $el  = this.$element
-    var val  = $el.is('input') ? 'val' : 'html'
-    var data = $el.data()
+    var d    = 'disabled';
+    var $el  = this.$element;
+    var val  = $el.is('input') ? 'val' : 'html';
+    var data = $el.data();
 
-    state += 'Text'
+    state += 'Text';
 
-    if (data.resetText == null) $el.data('resetText', $el[val]())
+    if (data.resetText == null) $el.data('resetText', $el[val]());
 
     // push to event loop to allow forms to submit
     setTimeout($.proxy(function () {
-      $el[val](data[state] == null ? this.options[state] : data[state])
+      $el[val](data[state] == null ? this.options[state] : data[state]);
 
       if (state == 'loadingText') {
-        this.isLoading = true
-        $el.addClass(d).attr(d, d).prop(d, true)
+        this.isLoading = true;
+        $el.addClass(d).attr(d, d).prop(d, true);
       } else if (this.isLoading) {
-        this.isLoading = false
-        $el.removeClass(d).removeAttr(d).prop(d, false)
+        this.isLoading = false;
+        $el.removeClass(d).removeAttr(d).prop(d, false);
       }
-    }, this), 0)
+    }, this), 0);
   }
 }
 
 +function ($) {
 
   Button.prototype.toggle = function () {
-    var changed = true
-    var $parent = this.$element.closest('[data-toggle="buttons"]')
+    var changed = true;
+    var $parent = this.$element.closest('[data-toggle="buttons"]');
 
     if ($parent.length) {
-      var $input = this.$element.find('input')
+      var $input = this.$element.find('input');
       if ($input.prop('type') == 'radio') {
-        if ($input.prop('checked')) changed = false
-        $parent.find('.active').removeClass('active')
-        this.$element.addClass('active')
+        if ($input.prop('checked')) changed = false;
+        $parent.find('.active').removeClass('active');
+        this.$element.addClass('active');
       } else if ($input.prop('type') == 'checkbox') {
-        if (($input.prop('checked')) !== this.$element.hasClass('active')) changed = false
-        this.$element.toggleClass('active')
+        if (($input.prop('checked')) !== this.$element.hasClass('active')) changed = false;
+        this.$element.toggleClass('active');
       }
-      $input.prop('checked', this.$element.hasClass('active'))
-      if (changed) $input.trigger('change')
+      $input.prop('checked', this.$element.hasClass('active'));
+      if (changed) $input.trigger('change');
     } else {
-      this.$element.attr('aria-pressed', !this.$element.hasClass('active'))
-      this.$element.toggleClass('active')
+      this.$element.attr('aria-pressed', !this.$element.hasClass('active'));
+      this.$element.toggleClass('active');
     }
   }
 }
