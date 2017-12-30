27042835e1413d3d506aeb0bfaba272531fef282
drfede <34274861+drfede@users.noreply.github.com>
2017-12-30 14:49:49 +0100

    2068 - 2100

--- bootstrap.js
+++ bootstrap.js
@@ -2065,39 +2065,39 @@ if (typeof jQuery === 'undefined') {
 
   ScrollSpy.DEFAULTS = {
     offset: 10
-  }
+  };
 
   ScrollSpy.prototype.getScrollHeight = function () {
-    return this.$scrollElement[0].scrollHeight || Math.max(this.$body[0].scrollHeight, document.documentElement.scrollHeight)
+    return this.$scrollElement[0].scrollHeight || Math.max(this.$body[0].scrollHeight, document.documentElement.scrollHeight);
   }
 }
 
 +function ($) {
   ScrollSpy.prototype.refresh = function () {
-    var that          = this
-    var offsetMethod  = 'offset'
-    var offsetBase    = 0
+    var that          = this;
+    var offsetMethod  = 'offset';
+    var offsetBase    = 0;
 
-    this.offsets      = []
-    this.targets      = []
-    this.scrollHeight = this.getScrollHeight()
+    this.offsets      = [];
+    this.targets      = [];
+    this.scrollHeight = this.getScrollHeight();
 
     if (!$.isWindow(this.$scrollElement[0])) {
-      offsetMethod = 'position'
-      offsetBase   = this.$scrollElement.scrollTop()
+      offsetMethod = 'position';
+      offsetBase   = this.$scrollElement.scrollTop();
     }
 
     this.$body
       .find(this.selector)
       .map(function () {
-        var $el   = $(this)
-        var href  = $el.data('target') || $el.attr('href')
-        var $href = /^#./.test(href) && $(href)
+        var $el   = $(this);
+        var href  = $el.data('target') || $el.attr('href');
+        var $href = /^#./.test(href) && $(href);
 
         return ($href
           && $href.length
           && $href.is(':visible')
-          && [[$href[offsetMethod]().top + offsetBase, href]]) || null
+          && [[$href[offsetMethod]().top + offsetBase, href]]) || null;
       })
       .sort(function (a, b) { return a[0] - b[0] })
       .each(function () {
