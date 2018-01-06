b6ad40052df53d6e914a8b07c19a03bb0064f29d
drfede <34274861+drfede@users.noreply.github.com>
2018-01-06 11:23:43 +0100

    prism

--- prism.js
+++ prism.js
@@ -222,7 +222,7 @@ Prism.languages.markup = {
 	doctype : /&lt;!DOCTYPE.+?>/,
 	cdata : /&lt;!\[CDATA\[[\w\W]*?]]>/i,
 	tag : {
-		pattern : /<\/?(?!\d)[^\s>\/=$<]+(?:\s+[^\s>\/=]+(?:=(?:("|')(?:\\[\s\S]|(?!\1)[^\\])*\1|[^\s'">=]+))?)*\s*\/?>/i,
+		pattern : /&lt;\/?[\w:-]*+\s*(?:\s+[\w:-]*+(?:=(?:("|')(\\?[\w\W])*?\1|\w*+))?\s*)*\/?>/gi,
 		inside : {
 			tag : {
 				pattern : /^&lt;\/?[\w:-]+/i,
